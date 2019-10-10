6ed6c04ba73043cb377946239d0243468a591a16
anthonyjchriste <achriste@hawaii.edu>
2019-10-10 10:56:28 -1000

    Multiply by another constant

--- pmotion.py
+++ pmotion.py
@@ -2,6 +2,6 @@ import math
 
 # This function returns the energy output of our perpetual motion machine.
 def pmotion():
-    energy = 42.0 * math.inf
+    energy = 42.0 * math.inf * 1
     return energy
 
