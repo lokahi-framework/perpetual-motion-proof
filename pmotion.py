a3253f495aa61b7fd25eef4cb7fd6ec924b85115
anthonyjchriste <achriste@hawaii.edu>
2019-10-10 12:17:48 -1000

    Update pmotion to fix #1
    Add code that fixes issue #1.

--- pmotion.py
+++ pmotion.py
@@ -3,5 +3,5 @@ import math
 # This function returns the energy output of our perpetual motion machine.
 def pmotion():
     energy = 42.0 + 1 * math.inf
-    # Add code to fix the issue
+    # Add some code to fix the issue
     return energy
