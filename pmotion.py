4f36d186d437bc674e11c195f0f607b5a72e6355
anthonyjchriste <achriste@hawaii.edu>
2019-10-10 12:02:00 -1000

    Update .gitignore and fix #1
    This commit updates the .gitignore and fixes issue #1.

--- pmotion.py
+++ pmotion.py
@@ -3,4 +3,5 @@ import math
 # This function returns the energy output of our perpetual motion machine.
 def pmotion():
     energy = 42.0 + 1 * math.inf
+    # Add code to fix the issue
     return energy
