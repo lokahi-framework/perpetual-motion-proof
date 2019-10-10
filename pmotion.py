c2a21a63f3bc2f49394bf2985fcde7de25a7f638
anthonyjchriste <achriste@hawaii.edu>
2019-10-10 12:42:58 -1000

    Add a new function to compute things

--- pmotion.py
+++ pmotion.py
@@ -2,8 +2,11 @@ import math
 
 # This function returns the energy output of our perpetual motion machine.
 def pmotion():
-    energy = 42.0 + 1 * math.inf
+    energy = 42.0 + 2 * math.inf
     return energy
 
 def something_else():
     return 2
+
+def another_thing():
+    return 3
