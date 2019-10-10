c47323b6adafbcdc4a48d5ff13061311bde57745
anthonyjchriste <achriste@hawaii.edu>
2019-10-10 10:57:13 -1000

    Update pmotion.py
    Add an addition by a constant.

--- pmotion.py
+++ pmotion.py
@@ -2,6 +2,6 @@ import math
 
 # This function returns the energy output of our perpetual motion machine.
 def pmotion():
-    energy = 42.0 * math.inf
+    energy = 42.0 + 1 * math.inf
     return energy
 
