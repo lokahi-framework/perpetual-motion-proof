2a3c51a4dd311c902c44f104f01b8306c9dd8e50
anthonyjchriste <achriste@hawaii.edu>
2019-10-10 12:26:12 -1000

    Revert "Revert "Update pmotion to fix #1""
    This reverts commit d90fe9f82e9ef36bb10ef439fc0712bfe843a934.

--- pmotion.py
+++ pmotion.py
@@ -3,7 +3,7 @@ import math
 # This function returns the energy output of our perpetual motion machine.
 def pmotion():
     energy = 42.0 + 1 * math.inf
-    # Add code to fix the issue
+    # Add some code to fix the issue
     return energy
 
 def something_else():
